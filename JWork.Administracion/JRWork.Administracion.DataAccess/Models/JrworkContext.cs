f94e5030c5a895e626ea335f6ede8d202559540c
MaterBit <julecci@gmail.com>
2024-09-05 01:39:02 -0500

    commit gitnore

--- JWork.Administracion/JRWork.Administracion.DataAccess/Models/JrworkContext.cs
+++ JWork.Administracion/JRWork.Administracion.DataAccess/Models/JrworkContext.cs
@@ -28,7 +28,6 @@ public partial class JrworkContext : DbContext
     public virtual DbSet<UnidadMedidum> UnidadMedida { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer("Data Source=LAPTOP-7VA8NVM3\\JULIANDB;Initial Catalog=JRWork;Integrated Security=True;TrustServerCertificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
